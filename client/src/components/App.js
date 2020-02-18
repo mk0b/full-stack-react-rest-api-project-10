3d938cec597641467c8ae7f8ffc21ee1569d5033
Megan Katherine O'Brien <megankob@gmail.com>
2020-02-18 12:55:16 -0500

    Adjusted css imports

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '../css/index.css';
 import axios from 'axios';
 
 //TODO: Components redirect users to the /error path when requests to 
@@ -21,20 +20,7 @@ apiCall();
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Project 10.</p>
     </div>
   );
 }
