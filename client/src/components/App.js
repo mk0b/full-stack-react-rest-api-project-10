526fdcfb24376c1ce9a598377f240e79bddf3ce4
Megan Katherine O'Brien <megankob@gmail.com>
2020-02-18 13:57:39 -0500

    Setting up Routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import '../css/index.css';
 import axios from 'axios';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+
+//importing components
+import Courses from './Courses';
+import CreateCourse from './CreateCourse';
+import UpdateCourse from './UpdateCourse';
+import CourseDetail from './CourseDetail';
+import UserSignIn from './UserSignIn';
+import UserSignUp from './UserSignUp';
+import UserSignOut from './UserSignOut';
+import NotFound from './NotFound';
+import Forbidden from './Forbidden';
+import UnhandledError from './UnhandledError';
 
 //TODO: Components redirect users to the /error path when requests to 
 //the REST API return a "500 Internal Server Error" HTTP status code.
@@ -19,9 +32,22 @@ apiCall();
 
 function App() {
   return (
-    <div className="App">
-      <p>Project 10.</p>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Courses}/>
+          <Route path="/courses/create" component={CreateCourse}/>
+          <Route path="/courses/:id/update" component={UpdateCourse}/>
+          <Route path="/courses/:id" component={CourseDetail}/>
+          <Route path="/signin" component={UserSignIn}/>
+          <Route path="/singup" component={UserSignUp}/>
+          <Route path="/signout" component={UserSignOut}/>
+          <Route path="/forbidden" component={Forbidden}/>
+          <Route path="/error" component={UnhandledError}/>
+          <Route path="/notfound" component={NotFound}/>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
