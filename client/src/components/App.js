fbf94d18f5d6cb60f0231bf5c8e5787b1d568e78
Megan Katherine O'Brien <megankob@gmail.com>
2020-02-18 14:06:27 -0500

    Working on courses component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 //importing components
 import Courses from './Courses';
-import CreateCourse from './CreateCourse';
+/* import CreateCourse from './CreateCourse';
 import UpdateCourse from './UpdateCourse';
 import CourseDetail from './CourseDetail';
 import UserSignIn from './UserSignIn';
@@ -13,7 +13,7 @@ import UserSignUp from './UserSignUp';
 import UserSignOut from './UserSignOut';
 import NotFound from './NotFound';
 import Forbidden from './Forbidden';
-import UnhandledError from './UnhandledError';
+import UnhandledError from './UnhandledError'; */
 
 //TODO: Components redirect users to the /error path when requests to 
 //the REST API return a "500 Internal Server Error" HTTP status code.
@@ -36,7 +36,7 @@ function App() {
       <div className="App">
         <Switch>
           <Route exact path="/" component={Courses}/>
-          <Route path="/courses/create" component={CreateCourse}/>
+{/*           <Route path="/courses/create" component={CreateCourse}/>
           <Route path="/courses/:id/update" component={UpdateCourse}/>
           <Route path="/courses/:id" component={CourseDetail}/>
           <Route path="/signin" component={UserSignIn}/>
@@ -44,7 +44,7 @@ function App() {
           <Route path="/signout" component={UserSignOut}/>
           <Route path="/forbidden" component={Forbidden}/>
           <Route path="/error" component={UnhandledError}/>
-          <Route path="/notfound" component={NotFound}/>
+          <Route path="/notfound" component={NotFound}/> */}
         </Switch>
       </div>
     </BrowserRouter>
--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -1,1 +1,18 @@
 //stateful class component
+import React, { Component, Fragment } from 'react';
+
+class Courses extends Component {
+    //state here?
+
+    //component did mount?
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Courses;
