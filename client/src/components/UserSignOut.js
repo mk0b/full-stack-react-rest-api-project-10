7ac518127ab00c2bea22ff82a985e832b32bfe28
Megan Katherine O'Brien <megankob@gmail.com>
2020-02-20 21:56:11 -0500

    Added placeholder

--- client/src/components/UserSignOut.js
+++ client/src/components/UserSignOut.js
@@ -5,3 +5,11 @@
 //(i.e. the list of courses).
 
 import React, { Fragment } from 'react';
+
+const UserSignOut = () => (
+    <Fragment>
+        <p>This is just a placeholder. Remove!</p>
+    </Fragment>
+);
+
+export default UserSignOut;
