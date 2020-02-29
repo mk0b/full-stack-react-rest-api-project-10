2b4d33bd0c18a4652e27cad10660a550c8444fa3
Megan Katherine O'Brien <megankob@gmail.com>
2020-02-28 23:05:21 -0500

    Update

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -44,6 +44,7 @@ function App() {
           <Route path="/forbidden" component={Forbidden} />
           <Route path="/signout" component={UserSignOutWithContext} />
           <Route path="/notfound" component={NotFound} />
+          <Route component={NotFound} />
         </Switch>
       </div>
     </BrowserRouter>
